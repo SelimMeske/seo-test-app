c3e284abcf2a58b05e15c37a9a2c5ec6eef46789
SelimMeske <mesic.selim@outlook.com>
2020-08-27 16:57:29 +0200

    seo test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PageOneComponent } from './page-one/page-one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageOneComponent
   ],
   imports: [
     BrowserModule,
